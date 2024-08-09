6de77af8f002163a8cdef6c1b49d022afde2e1c5
arctozx <mattzkie07@gmail.com>
2024-08-09 23:08:19 +0800

    Update server routing and build configurations

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,32 @@
+import dotenv from "dotenv";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+dotenv.config({ path: path.join(__dirname, ".env") });
+
 import express from "express";
 import mongoose from "mongoose";
-import booksRoute from "./routes/booksRoute.js"
-import cors from "cors"
-import dotenv from "dotenv";
+import booksRoute from "./routes/booksRoute.js";
+import cors from "cors";
 
-dotenv.config();
-
-const PORT = process.env.PORT || 4000
-const mongoDBURL = process.env.mongoDBURL
+const PORT = process.env.PORT || 4000;
+const mongoDBURL = process.env.mongoDBURL;
 
 const app = express();
-app.use(express.json());
-app.use(cors())
 
-app.use("/books", booksRoute)
+app.use(express.json());
+app.use(cors());
+
+app.use("api/books", booksRoute);
+
+app.use(express.static(path.join(__dirname, "..", "/frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "frontend", "dist", "index.html"));
+});
 
 mongoose
   .connect(mongoDBURL)
